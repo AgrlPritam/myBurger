ab758a8993e699d5ece9abdbd8bdaeaf3b88227a
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-11-06 17:40:38 +0530

    Adding state to dynamically control Ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,22 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    //For converting ingredients objects from state in burger builder to array
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_,i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    }).reduce((arr,el) => {
+        return arr.concat(el)
+    },[]);  
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Your Taste Buds Want You To Fill Me!</p>
+    }
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="salad" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
