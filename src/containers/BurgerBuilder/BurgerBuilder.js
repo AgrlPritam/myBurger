037964e35df72014da5cb3b737a0acc1fad22f46
Pritam Agrawal <pritamagrawal95@gmail.com>
2020-12-05 08:44:09 +0530

    Wiring up spinner and orderSummary loader

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import Spinner from '../../components/UI/Spinner/Spinner'
 import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
@@ -23,7 +24,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 40,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseState(ingredients)  {
@@ -76,6 +78,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('You can Continue!!')
+        this.setState({loading:true})
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -90,9 +93,11 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'Fastest'
         }
-        axios.post('/orders.json', order)      //For firebase database using axios each endpoints need a .json at end. MongoDB doesn't need this
-            .then(response => console.log(response))
-            .catch(error => console.log(error))
+        // axios.post('/orders.json', order)      //For firebase database using axios each endpoints need a .json at end. MongoDB doesn't need this
+        //     .then(response => 
+        //         this.setState({loading:false, purchasing:false}))
+        //     .catch(error => 
+        //         this.setState({loading: false, purchasing:false}))
     }
     
     render() {
@@ -103,15 +108,20 @@ class BurgerBuilder extends Component {
         for(let key in disabledInfo){
             disabledInfo[key] = disabledInfo[key] <= 0
         }
+
+        let orderSummary = <OrderSummary 
+                                ingredients={this.state.ingredients} 
+                                purchaseCancelled={this.purchaseCancelHandler}
+                                purchaseContinued={this.purchaseContinueHandler}
+                                price={this.state.totalPrice.toFixed(2)}
+                            />
+        if(this.state.loading) {
+            orderSummary = <Spinner />
+        }
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients} 
-                        purchaseCancelled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}
-                        price={this.state.totalPrice.toFixed(2)}
-                    />
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
